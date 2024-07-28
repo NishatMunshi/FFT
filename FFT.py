132bab660cc1b56a09c30c0c5c30f3f5a57459d2
Nishat Munshi <nishatmunshi9@gmail.com>
2024-07-28 20:08:07 +0530

    added a python script and improved the C++ one

---
+++ FFT.py
@@ -0,0 +1,38 @@
+# expects samples size be a power of 2
+def FFT_recursive(samples, omega):
+    n = len(samples)
+
+    if n == 1:
+        return samples
+
+    samples_eve = samples[0::2]
+    samples_odd = samples[1::2]
+
+    ret_eve = FFT_recursive(samples_eve, omega * omega)
+    ret_odd = FFT_recursive(samples_odd, omega * omega)
+
+    ret = [None] * n
+    root = 1 + 0j
+    for i in range(n // 2):
+        ret[i + 0 // 2] = ret_eve[i] + root * ret_odd[i]
+        ret[i + n // 2] = ret_eve[i] - root * ret_odd[i]
+        root *= omega
+
+    return ret
+
+import cmath
+
+def is_power_of_2(x):
+    return (x > 0) and not (x & (x - 1))
+
+def FFT(samples):
+    if not is_power_of_2(len(samples)):
+        raise ValueError("Samples size is not a power of two.\n")
+    return FFT_recursive(samples, cmath.exp(0 - 2j * cmath.pi / len(samples)))
+
+def iFFT(samples):
+    if not is_power_of_2(len(samples)):
+        raise ValueError("Samples size is not a power of two.\n")
+    ifft = FFT_recursive(samples, cmath.exp(0 + 2j * cmath.pi / len(samples)))
+    ifft[:] = [x / len(samples) for x in ifft]
+    return ifft
--- src/FFT.cpp
+++ src/FFT.cpp
@@ -3,17 +3,10 @@
 #include <algorithm>
 #include <stdexcept>
 
-static bool is_power_of_2(std::size_t _num) {
-    return (_num > 0) and not(_num bitand (_num - 1));
-}
-
 static std::vector<std::complex<double>>
-FFT(std::vector<std::complex<double>> const &_samples,
+FFT(std::vector<std::complex<double>> const& _samples,
     std::complex<double> const _omega) {
     auto const n = _samples.size();
-    if (not is_power_of_2(n)) {
-        throw std::invalid_argument("Input size is not a power of 2\n");
-    }
 
     // base case
     if (n == 1) {
@@ -34,25 +27,36 @@ FFT(std::vector<std::complex<double>> const &_samples,
 
     // merge the two halves
     std::vector<std::complex<double>> result(n);
+    auto root_of_unity = std::complex<double>{ 1, 0 };
     for (std::size_t index = 0; index < n / 2; ++index) {
-        auto const root_of_unity = std::pow(_omega, index);
         result.at(index + 0 / 2) = return_eve.at(index) + root_of_unity * return_odd.at(index);
         result.at(index + n / 2) = return_eve.at(index) - root_of_unity * return_odd.at(index);
+        root_of_unity *= _omega;
     }
 
     return result;
 }
 
-std::vector<std::complex<double>> FFT(std::vector<std::complex<double>> const &_samples) {
+static bool is_power_of_2(std::size_t _num) {
+    return (_num > 0) and not(_num bitand (_num - 1));
+}
+
+std::vector<std::complex<double>> FFT(std::vector<std::complex<double>> const& _samples) {
+    if (not is_power_of_2(_samples.size())) {
+        throw std::invalid_argument("Input size is not a power of 2\n");
+    }
     using namespace std::literals::complex_literals;
     return FFT(_samples, std::exp(2.0i * M_PI / double(_samples.size())));
 }
 
-std::vector<std::complex<double>> iFFT(std::vector<std::complex<double>> const &_samples) {
+std::vector<std::complex<double>> iFFT(std::vector<std::complex<double>> const& _samples) {
     auto const n = _samples.size();
+    if (not is_power_of_2(n)) {
+        throw std::invalid_argument("Input size is not a power of 2\n");
+    }
     using namespace std::literals::complex_literals;
     auto ifft = FFT(_samples, std::exp(-2.0i * M_PI / double(n)));
-    std::for_each(ifft.begin(), ifft.end(), [&](std::complex<double> &_z) { _z /= double(n); });
+    std::for_each(ifft.begin(), ifft.end(), [&](std::complex<double>& _z) { _z /= double(n); });
 
     return ifft;
 }
