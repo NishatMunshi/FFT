7aeb45e8aa15abdc0ddc61a291b81ee979717f5e
Nishat Munshi <nishatmunshi9@gmail.com>
2024-08-22 08:24:50 +0530

    drawing only magnitude

--- src/main.cpp
+++ src/main.cpp
@@ -5,66 +5,46 @@
 
 #include "../include/FFT.hpp"
 
-constexpr double rect(double i, double L) {
-    if (std::abs(i) < std::abs(L)) {
+constexpr double
+rect(double t, double T) {
+    if (std::abs(t) <= std::abs(T)) {
         return 1;
     }
     return 0;
 }
 
+constexpr double
+h(double t) {
+    return rect(t, 50) * (3 * std::cos(t) + std::sin(2 * t));
+}
+
 #define WIN_DIM_VER 800
-#define WIN_DIM_HOR (2 * WIN_DIM_VER)
+#define WIN_DIM_HOR (WIN_DIM_VER)
 
-void draw_fft_mag(std::vector<std::complex<double>> const &fft, sf::RenderWindow &window) {
+void draw_mag(std::vector<std::complex<double>> const &values,
+              sf::RenderWindow &window) {
     sf::Vertex point{{0, 0}, sf::Color::Yellow};
-    float x_step = (float)WIN_DIM_HOR / 2 / fft.size();
-    auto largest_mag = std::abs(*std::max_element(fft.cbegin(),
-                                                  fft.cend(),
+    float x_step = float(WIN_DIM_HOR) / values.size();
+    auto largest_mag = std::abs(*std::max_element(values.cbegin(),
+                                                  values.cend(),
                                                   [](std::complex<double> w, std::complex<double> z) { return std::abs(w) < std::abs(z); }));
-    float y_scalar = WIN_DIM_VER / largest_mag;
+    float y_scalar = 3 * WIN_DIM_VER / 4 / largest_mag;
 
-    for (std::size_t index = fft.size() / 2; const auto &value : fft) {
-        if (index > fft.size()) index = 0;
+    for (std::size_t index = values.size() / 2; const auto &value : values) {
+        if(index > values.size()) index = 0;
         auto mag = std::abs(value);
 
         point.position = {(index++) * x_step,
-                          WIN_DIM_VER - y_scalar * (float)mag};
+                          WIN_DIM_VER - y_scalar * float(mag)};
 
         window.draw(&point, 1, sf::Points);
     }
-}
-
-void draw_fft_phase(std::vector<std::complex<double>> const &fft, sf::RenderWindow &window) {
-    sf::Vertex point{{0, 0}, sf::Color::Green};
-    float x_step = (float)WIN_DIM_HOR / 2 / fft.size();
-    auto largest_arg = std::arg(*std::max_element(fft.cbegin(),
-                                                  fft.cend(),
-                                                  [](std::complex<double> w, std::complex<double> z) { return std::arg(w) < std::arg(z); }));
-    float y_scalar = WIN_DIM_VER / 4 / largest_arg;
-
-    for (std::size_t index = fft.size() / 2; const auto &value : fft) {
-        if (index > fft.size()) index = 0;
-        auto argument = std::arg(value);
-
-        point.position = {(index++) * x_step + WIN_DIM_HOR / 2,
-                          WIN_DIM_VER / 2 - y_scalar * (float)argument};
-
-        window.draw(&point, 1, sf::Points);
-    }
-}
-
-void draw_divider(sf::RenderWindow &window) {
-    sf::VertexArray lines(sf::LinesStrip, 2);
-    lines[0].position = sf::Vector2f(WIN_DIM_HOR / 2, 0);
-    lines[1].position = sf::Vector2f(WIN_DIM_HOR / 2, WIN_DIM_VER);
-
-    window.draw(lines);
 }
 
 int main() {
     std::vector<std::complex<double>> samples;
     for (int32_t i = INT16_MIN; i <= INT16_MAX; ++i) {
-        samples.push_back(rect(i, 10));
+        samples.push_back(h(i));
     }
 
     auto const fft = FFT(samples);
@@ -79,9 +59,7 @@ int main() {
         }
 
         window.clear();
-        draw_fft_mag(fft, window);
-        draw_divider(window);
-        draw_fft_phase(fft, window);
+        draw_mag(fft, window);
         window.display();
     }
     return 0;
